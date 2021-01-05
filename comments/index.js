15c3681b74d3337a54f6a983d33016f9ab35e401
KazikKluz <kazik.kluz@gmail.com>
2021-01-05 21:14:57 +0000

    receiving events 30

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
-  await axios.post("https://localhost:4005/events", {
+  await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -33,6 +33,11 @@ app.post("/posts/:id/comments", async (req, res) => {
     },
   });
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -25,7 +25,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005", {
+  await axios.post("http://localhost:4005/events", {
     type: "Post Created",
     data: {
       id,
@@ -34,6 +34,11 @@ app.post("/posts", async (req, res) => {
   });
 
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
